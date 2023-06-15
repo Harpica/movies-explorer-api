c6a9720c86cd8b330461152e07ffb770bb9f27be
Harpica <harpy.ka@gmail.com>
2023-06-15 14:32:09 +0400

    fix: protect /signout with auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import { celebrate } from 'celebrate';
 import { signIn, signOut, signUp } from '../controllers/auth';
 import validator from '../utils/validator';
+import authChecker from '../middlewares/authChecker';
 
 const auth = Router();
 
 auth.post('/signup', celebrate(validator.auth.signup), signUp);
 auth.post('/signin', celebrate(validator.auth.signin), signIn);
-auth.post('/signout', signOut);
+auth.post('/signout', authChecker, signOut);
 
 export default auth;
